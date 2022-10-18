2792ed11d3540c8bc5ed0f1a70e5aabc9e5b2ea9
LC1301309 <pat.suarez@duocuc.cl>
2022-10-18 11:31:04 -0300

    i

---
+++ dist/src/api/viajes-pasajero/routes/viajes-pasajero.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * viajes-pasajero router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::viajes-pasajero.viajes-pasajero');
